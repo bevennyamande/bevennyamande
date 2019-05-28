8752e44c2f30ac75adee3f660249f23d9ddd4723
bevennyamande <bevennyamande@gmail.com>
2019-05-28 20:07:35 +0200

    commits

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Beven Nyamande | Homepage</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="static/css/werkzeug.css">
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-lg-2"></div>   
+        <div class="col-lg-8"><div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+          
+            <div class=" navbar-collapse" id="navbarsExample10">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                       <a class="nav-link" href=""><img src="static/img/logo.png" style="height: 50px;"></a></a>
+                    </li>
+
+                </ul>
+                         <ul class="nav pull-right ml-lg-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">about me</a>
+                            </li>
+                        </ul> 
+            </div>
+
+        </nav>
+        <div class="content">
+             <div class="jumbotron">
+            <h3>Hello,</h3>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+                <a class="btn btn-outline-success btn-sm" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+               <div class="media container-fluid">
+                    <div class="media-body">
+                        <h5 class="mt-0">Professional</h5>
+                        <p class="text-justify">We value our customers and hence we have a dedicated team of professionals at your service</p>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-lg-4">
+                <div class="media container-fluid">
+                    <div class="media-body">
+                        <h5 class="mt-0">Timely</h5>
+                        <p class="text-justify">Time is of essence. We strive to achieve all of your objectives and projects timeously</p>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-lg-4">
+                <div class="media container-fluid">
+                    <div class="media-body">
+                        <h5 class="mt-0">Excellence</h5>
+                        <p class="text-justify"> Lets attain excellence together. We ship excellent products for the happiness of our clients</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        </div>
+       
+    </div>
+    </div>
+</div>   
+        <div class="col-lg-2"></div>   
+
+    </div>
+
+</body>
+
+</html>
